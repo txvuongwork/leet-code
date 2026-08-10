cebdd59bda6fcb01f976daa2ec9a39bfca5c48b9
txvuongwork <txvuong.work@gmail.com>
2026-08-10 14:45:14 +0700

    DONE: two sum

--- two-sum.ts
+++ two-sum.ts
@@ -1,15 +1,33 @@
-export function twoSum(nums: number[], target: number): number[] {
-    const pairs: Map<number, number> = new Map();
+// Base solution
+// export function twoSum(nums: number[], target: number): number[] {
+//     const pairs: Map<number, number> = new Map();
 
-    nums.forEach((num, idx) => {
-        pairs.set(num, idx);
-    });
+//     nums.forEach((num, idx) => {
+//         pairs.set(num, idx);
+//     });
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const remainValueIdx = pairs.get(target - nums[i]);
+//         if (remainValueIdx !== undefined && remainValueIdx !== i) {
+//             return [i, remainValueIdx];
+//         }
+
+//         pairs.set(nums[i], i);
+//     }
+
+//     return [];
+// }
+
+// Optimized solution with one pass & change variable names
+export function twoSum(nums: number[], target: number): number[] {
+    const seen: Map<number, number> = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        const remainValueIdx = pairs.get(Math.abs(target - nums[i]));
-        if (remainValueIdx && remainValueIdx !== i) {
-            return [i, remainValueIdx];
+        const complementIdx = seen.get(target - nums[i]);
+        if (complementIdx !== undefined) {
+            return [complementIdx, i];
         }
+        seen.set(nums[i], i);
     }
 
     return [];
