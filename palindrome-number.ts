c47db2ae6cb9dfbdefcd54d4309b872f1be6adb1
txvuongwork <txvuong.work@gmail.com>
2026-08-11 08:57:39 +0700

    DONE: palindrome number

--- palindrome-number.ts
+++ palindrome-number.ts
@@ -1,22 +1,16 @@
-// Base solution
+// Edge case nếu convert sang string 10 -> 01
+// Complexity: O(log10(x)) time, O(1) space, vì loop qua n digit, n = log10(x)
+
 export function isPalindrome(x: number): boolean {
     if (x < 0) return false;
 
-    const digits: number[] = [];
-    while (x > 9) {
-        const digit = x % 10;
-        x = Math.floor(x / 10);
-        digits.push(digit);
-    }
-    digits.push(x);
-
-    let left = 0,
-        right = digits.length - 1;
-    while (left < right) {
-        if (digits[left] !== digits[right]) return false;
-        left++;
-        right--;
+    let remaining = x;
+    let reversed = 0;
+    while (remaining > 0) {
+        const digit = remaining % 10;
+        remaining = Math.floor(remaining / 10);
+        reversed = reversed * 10 + digit;
     }
 
-    return true;
+    return x === reversed;
 }
